42e135dbdb3ac909f3224db5482c6825f4caea57
beefchimi <curtdulmage@gmail.com>
2024-10-10 16:23:00 -0400

    :bug: [SectionTracker] Move away from IntersectionObserver

--- src/scripts/SectionTracker.ts
+++ src/scripts/SectionTracker.ts
@@ -1,57 +1,144 @@
+import {calcProgress} from 'beeftools';
+
+interface SectionBox {
+  id: string;
+  order: number;
+  top: number;
+  bottom: number;
+  height: number;
+  startPercentage: number;
+  heightPercentage: number;
+}
+
+// NOTE: We are not using an IntersectionObserver for this, as it
+// was unreliable in tracking the correct on-screen section.
+
 // TODO: Since we do not actually render <Navigation /> on `mobile`, we
 // should use a `matchMedia` listener to conditionally execute this code.
 export class SectionTracker {
   #sections: NodeListOf<Element>;
   #fallback: string;
-  #observer: IntersectionObserver | undefined;
+
+  #running = false;
+  #rafId = 0;
+  #rafTicking = false;
 
   constructor(sectionsSelector = 'section', activeFallback = 'home') {
     this.#sections = document.querySelectorAll(sectionsSelector);
     this.#fallback = activeFallback;
   }
 
+  get docHeight() {
+    return document.documentElement.scrollHeight;
+  }
+
+  get viewHeight() {
+    return document.documentElement.clientHeight;
+  }
+
+  get isScrollOutsideRange() {
+    const winY = window.scrollY;
+    return winY < 0 || winY > this.docHeight;
+  }
+
+  get sectionBoxes() {
+    const sectionsArray = Array.from(this.#sections);
+
+    return sectionsArray.reduce<SectionBox[]>((collection, section, index) => {
+      const id = section.getAttribute('data-section') ?? `section-${index}`;
+      const height = section.scrollHeight;
+
+      const previous = collection[index - 1];
+      const prevBottom = previous?.bottom ?? 0;
+
+      // Some of the top/bottom/height values will not be correct
+      // if negative offsets are used.
+      return [
+        ...collection,
+        {
+          id,
+          order: index,
+          top: prevBottom,
+          bottom: prevBottom + height,
+          height,
+          startPercentage: calcProgress(prevBottom, {
+            max: this.docHeight,
+            round: true,
+          }),
+          heightPercentage: calcProgress(height, {
+            max: this.docHeight,
+            round: true,
+          }),
+        },
+      ];
+    }, []);
+  }
+
+  get activeSection() {
+    const scrollY = window.scrollY;
+    const maxScrollY = this.docHeight - this.viewHeight;
+    const viewMid = Math.floor(this.viewHeight / 2) + scrollY;
+
+    if (scrollY >= maxScrollY) {
+      const finalSection = this.sectionBoxes[this.sectionBoxes.length - 1];
+      return finalSection.id;
+    }
+
+    const active = this.sectionBoxes.find((section) => {
+      const mostlyVisible = section.top <= viewMid && section.bottom > viewMid;
+      return mostlyVisible;
+    });
+
+    return active?.id ?? this.#fallback;
+  }
+
   run() {
     if (!this.#sections.length) return;
 
-    // If we already have an active IntersectionObserver...
-    // tear it down before continuing.
-    if (this.#observer) this.teardown();
+    // If already running... teardown before continuing.
+    if (this.#running) this.teardown();
 
-    this.#observer = new IntersectionObserver(this.#handleIntersect, {
-      root: null,
-      rootMargin: '0px',
-      // `threshold` value determined based on browser testing.
-      // If <section /> heights change, we may want to revisit this value.
-      threshold: 0.5,
-    });
-
-    this.#sections.forEach((element) => {
-      this.#observer?.observe(element);
-    });
+    this.#registerEventListeners(true);
+    this.#running = true;
   }
 
   teardown() {
-    this.#observer?.disconnect();
-    document.documentElement.dataset.activeSection = '';
+    // window.removeEventListener('hashchange', this.#updateUrl);
+    window.removeEventListener('scroll', this.#handleUpdateEvent);
+    window.removeEventListener('resize', this.#handleUpdateEvent);
+
+    cancelAnimationFrame(this.#rafId);
+    this.#rafId = 0;
+
+    document.documentElement.dataset.activeSection = this.#fallback;
   }
 
-  #handleIntersect: IntersectionObserverCallback = (entries) => {
-    // console.log('entries', entries);
+  #registerEventListeners(immediate = false) {
+    // TODO: Considering updating the hash URL.
+    // window.addEventListener('hashchange', this.#updateUrl, false);
 
-    entries.forEach(({target, isIntersecting}) => {
-      // console.log(target.id, isIntersecting, intersectionRatio);
-
-      if (isIntersecting) {
-        const targetSection = target.getAttribute('data-section');
-        document.documentElement.dataset.activeSection =
-          targetSection ?? this.#fallback;
-
-        // TODO: Consider updating the URL Hash
-        // const targetId = target.id;
-        // history.replaceState(null, '', urlHash);
-
-        // console.log('currently intersecting', targetSection);
-      }
+    window.addEventListener('scroll', this.#handleUpdateEvent, {
+      passive: true,
     });
+
+    window.addEventListener('resize', this.#handleUpdateEvent, {
+      passive: true,
+    });
+
+    if (immediate) this.#update();
+  }
+
+  #handleUpdateEvent = () => {
+    if (this.#rafTicking) return;
+
+    this.#rafId = requestAnimationFrame(this.#update);
+    this.#rafTicking = true;
+
+    this.#update();
+  };
+
+  #update = () => {
+    document.documentElement.dataset.activeSection = this.activeSection;
+    this.#rafTicking = false;
   };
 }
